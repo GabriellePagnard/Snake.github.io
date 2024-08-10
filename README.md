623e1cad8ef0f0a46ed493ea0c75641a86a2a2ee
Gabrielle Pagnard <gpagnard@gmail.com>
2024-08-10 17:03:41 +0200

    Ajout commentaires et read me

--- README.md
+++ README.md
@@ -1,3 +1,138 @@
-# Snake.github.io
+# 🐍🎮 Jeu du Snake 🎮🐍
 
-Pour jouer : [SNAKE](https://gabriellepagnard.github.io/Snake.github.io/).
+<br>
+
+## 📜 Présentation
+
+Bienvenue dans le projet Snake Game ! Ce classique intemporel revient dans une version modernisée et optimisée pour le web, offrant une expérience fluide et addictive. Avec une interface élégante et des fonctionnalités captivantes, ce jeu saura vous faire replonger dans l'univers des jeux rétro tout en profitant des avantages des technologies modernes.     
+
+<br>
+<br>
+
+## ✨ Fonctionnalités Principales
+
+🎮 Gameplay classique : Replongez dans l'expérience du jeu Snake, avec des graphismes modernisés et une jouabilité fluide   
+🏆 Système de score dynamique : Accumulez des points en mangeant des pommes et voyez votre score augmenter en temps réel   
+⏩ Augmentation de la vitesse : Le jeu devient de plus en plus rapide à chaque pomme mangée, rendant le défi encore plus excitant   
+⏸️ Mode Pause : Mettez le jeu en pause à tout moment pour reprendre plus tard, sans perdre votre progression   
+💻 Interface responsive : Le jeu s'adapte à toutes les tailles d'écran, vous permettant de jouer aussi bien sur mobile que sur ordinateur   
+🎨 Design personnalisé : Profitez d'une interface visuelle agréable avec un arrière-plan stylisé et des fenêtres pop-up élégantes.   
+
+<br>
+<br>
+
+## 📸 Aperçu
+
+Voici à quoi ressemble l'application :   
+
+![screenshot démarrage](./images/screenshot_demarrage.jpg)   
+
+![screenshot démarrage joueurs](./images/screenshot_jeu.jpg)   
+
+![screenshot jeu en cours](./images/screenshot_fin_jeu.jpg)   
+
+[Démo vidéo du projet 🎥](https://www.youtube.com/watch?v=dOzv-hM1oEQ)
+
+<br>
+<br>
+
+## 🌐 Lien direct
+
+Pour jouer : [SNAKE 🐍](https://gabriellepagnard.github.io/Snake.github.io/)   
+
+
+<br>
+<br>
+
+## 🔧 Technologies Utilisées
+
+- HTML 🏗️    
+- CSS 🎨   
+- JavaScript ⚙️   
+
+<br>
+<br>
+
+## 📂 Fichiers du Projet
+
+- index.html : Structure HTML de la page web.   
+- style.css : Styles CSS pour l'apparence de l'application   
+- app.js : Script JavaScript pour la logique de recherche, la gestion des résultats et les interactions avec l'API    
+- images/ : Dossier contenant les images et les icônes (y compris le favicon et les captures d'écran)   
+
+<br>
+<br>
+
+
+## 🛠️ Installation et Utilisation
+
+Pour démarrer avec Calculatrice, suivez ces étapes simples :   
+
+➡️ Clonez le Répertoire :   
+
+`git clone https://github.com/votre-utilisateur/Snake.github.io.git`   
+
+➡️ Accédez au Répertoire :   
+
+`cd snake`   
+
+➡️ Ouvrez le Fichier "index.html" dans votre navigateur pour voir l'application en action   
+
+➡️ Personnalisez le Code selon vos besoins ou apportez des contributions !   
+
+<br>
+<br>
+
+
+## 🏗️ Comment Contribuer
+
+Vous souhaitez contribuer ? 🎉 Voici comment faire :   
+
+### Créer une Branche 🌿
+
+➡️ Pour chaque nouvelle fonctionnalité ou correction, créez une nouvelle branche :   
+
+`git checkout -b nom-de-la-branche`   
+
+### Faire vos Changements ✏️   
+
+Apportez les modifications nécessaires et committez-les :   
+
+`git add .`   
+`git commit -m "Description des changements"`   
+
+### Pousser les Changements ⬆️   
+
+Envoyez vos modifications à GitHub 📨 :
+
+`git push origin nom-de-la-branche`   
+
+### Créer une Pull Request 🔄   
+
+Allez sur GitHub, ouvrez une Pull Request et expliquez vos modifications   
+
+<br>
+<br>
+
+## 🤝 Contributeurs/trices
+
+Gabrielle Pagnard 🧑‍💻 - Créatrice du projet
+
+<br>
+<br>
+
+## 📬 Contact
+
+Pour toute question ou suggestion, n'hésitez pas à me contacter :   
+
+Email : gpagnard@gmail.com   
+GitHub : [GabriellePagnard](https://github.com/GabriellePagnard)     
+
+<br>
+<br>
+<br>
+
+Merci d'avoir visité le projet Snake ! 🚀   
+
+Nous espérons que vous vous amuserez autant en jouant au Jeu du Snake que nous avons eu à le développer ! 🎉 
+N'hésitez pas à laisser un ⭐ si vous aimez ce projet !  
--- app.js
+++ app.js
@@ -1,28 +1,50 @@
 const app = {
 
+    /**
+     * Vitesse initiale du jeu (en millisecondes).
+     * @type {number}
+     */
     vitesse: 200,
+
+    /**
+     * Indique si le jeu est en pause.
+     * @type {boolean}
+     */
     paused: false,
+
+    /**
+     * ID de l'animation courante (utilisé pour annuler l'animation).
+     * @type {number|null}
+     */
     animationId: null,
 
+    /**
+     * Démarre l'animation du jeu.
+     * Utilise un setTimeout pour créer une boucle d'animation récursive.
+     * @function
+     */
     animation() {
         if (app.paused) {
             return;
         }
         setTimeout(() => {
-            app.nettoieCanvas();
-            app.dessinePomme()
-            app.faireAvancerSerpent();
-            if (app.finDuJeu()) {
-                console.log('3')
-                app.afficherGameOver(score);
+            app.nettoieCanvas();  // Nettoie le canvas
+            app.dessinePomme();  // Dessine la pomme sur le canvas
+            app.faireAvancerSerpent();  // Fait avancer le serpent
+            if (app.finDuJeu()) {  // Vérifie si le jeu est terminé
+                app.afficherGameOver(score);  // Affiche l'écran Game Over si le jeu est terminé
                 return;
             }
-            app.dessineLeSerpent();
-            // recursion
-            app.animation();
+            app.dessineLeSerpent();  // Dessine le serpent
+            app.animation();  // Appelle récursivement l'animation
         }, app.vitesse);
     },
 
+    /**
+     * Nettoie le canvas pour redessiner.
+     * Remplit le canvas avec un fond blanc et une bordure noire.
+     * @function
+     */
     nettoieCanvas() {
         ctx.fillStyle = "white";
         ctx.strokeStyle = "black";
@@ -30,28 +52,40 @@ const app = {
         ctx.strokeRect(0, 0, canvas.width, canvas.height);
     },
 
+    /**
+     * Dessine une partie du serpent.
+     * @param {Object} morceau - Un morceau du serpent à dessiner.
+     * @param {number} morceau.x - La position x du morceau.
+     * @param {number} morceau.y - La position y du morceau.
+     */
     dessineLesMorceaux(morceau) {
         ctx.fillStyle = "#00a30e";
         ctx.strokeStyle = "#035811";
         ctx.fillRect(morceau.x, morceau.y, 10, 10);
         ctx.strokeRect(morceau.x, morceau.y, 10, 10);
-
     },
 
+    /**
+     * Dessine l'ensemble du serpent.
+     * Parcourt chaque morceau du serpent et le dessine.
+     * @function
+     */
     dessineLeSerpent() {
         snake.forEach(morceau => {
             app.dessineLesMorceaux(morceau);
-        })
+        });
     },
 
-
-
+    /**
+     * Fait avancer le serpent dans la direction actuelle.
+     * Ajoute une nouvelle tête au serpent et retire la queue sauf si le serpent mange une pomme.
+     * @function
+     */
     faireAvancerSerpent() {
         const head = { x: snake[0].x + vx, y: snake[0].y + vy };
         snake.unshift(head);
 
-
-        const serpentMangePomme = snake[0].x === pommeX && snake[0].y === pommeY;  // Vérifi si la tête du serpent est sur la pomme
+        const serpentMangePomme = snake[0].x === pommeX && snake[0].y === pommeY;  // Vérifie si le serpent mange la pomme
 
         if (serpentMangePomme) {
             score += 10;
@@ -59,17 +93,19 @@ const app = {
             if (scoreEl) {
                 scoreEl.innerHTML = score;
             }
-            app.creerPomme();
-            app.vitesse -= 5; // Permet d'augmenter la vitesse du serpent automatiquement plus il mange de pommes
+            app.creerPomme();  // Crée une nouvelle pomme
+            app.vitesse -= 5;  // Augmente la vitesse du serpent
         } else {
-            snake.pop();
+            snake.pop();  // Retire la dernière partie du serpent si aucune pomme n'est mangée
         }
-
     },
 
+    /**
+     * Change la direction du serpent en fonction des touches de direction.
+     * Empêche le serpent de faire demi-tour.
+     * @param {KeyboardEvent} event - L'événement keydown.
+     */
     changerDirection(event) {
-        //console.log(event)
-
         const FLECHE_GAUCHE = 37;
         const FLECHE_DROITE = 39;
         const FLECHE_ENHAUT = 38;
@@ -82,63 +118,76 @@ const app = {
         const adroite = vx === 10;
         const agauche = vx === -10;
 
-
-        // Pour interdir le retournement du serpent
-        if (direction === FLECHE_GAUCHE && !adroite) { vx = -10; vy = 0 }
-        if (direction === FLECHE_ENHAUT && !descendre) { vx = 0; vy = -10 }
-        if (direction === FLECHE_DROITE && !agauche) { vx = 10; vy = 0 }
-        if (direction === FLECHE_ENBAS && !monter) { vx = 0; vy = 10 }
-
+        // Empêche le serpent de faire demi-tour
+        if (direction === FLECHE_GAUCHE && !adroite) { vx = -10; vy = 0; }
+        if (direction === FLECHE_ENHAUT && !descendre) { vx = 0; vy = -10; }
+        if (direction === FLECHE_DROITE && !agauche) { vx = 10; vy = 0; }
+        if (direction === FLECHE_ENBAS && !monter) { vx = 0; vy = 10; }
     },
 
+    /**
+     * Ajoute un écouteur d'événements pour détecter les touches de direction.
+     * Gère les mouvements du serpent avec les touches du clavier.
+     * @function
+     */
     handleKeydown() {
         document.addEventListener('keydown', app.changerDirection);
     },
 
+    /**
+     * Initialise les variables du jeu.
+     * Configure le canvas et initialise les variables de jeu comme le serpent, la vitesse, etc.
+     * @function
+     */
     initialiseJeu() {
         canvas = document.getElementById('canvas');
         ctx = canvas.getContext('2d');
 
-        // variables
-
-        // Vitesse sur X
+        // Initialisation des variables de mouvement
         vx = 10;
-
-        // Vitesse sur Y
         vy = 0;
 
-        // PommeX
+        // Position de la pomme
         pommeX = 0;
-
-        // PommeY
         pommeY = 0;
 
-        // Score
+        // Score initial
         score = 0;
 
+        // Corps initial du serpent
         snake = [{ x: 140, y: 150 }, { x: 130, y: 150 }, { x: 120, y: 150 }, { x: 110, y: 150 }];
     },
 
+    /**
+     * Génère un nombre aléatoire pour positionner la pomme.
+     * @returns {number} - Position aléatoire.
+     */
     random() {
-
         return Math.round((Math.random() * 290) / 10) * 10;
     },
 
+    /**
+     * Crée une nouvelle pomme à une position aléatoire.
+     * Vérifie que la pomme ne se superpose pas au serpent.
+     * @function
+     */
     creerPomme() {
-
         pommeX = app.random();
         pommeY = app.random();
 
         snake.forEach(part => {
-
-            app.serpentSurPomme = part.x == pommeX && part.y == pommeY;
-
+            app.serpentSurPomme = part.x === pommeX && part.y === pommeY;
             if (app.serpentSurPomme) {
-                app.creerPomme();
+                app.creerPomme();  // Recrée une pomme si elle apparaît sur le serpent
             }
-        })
+        });
     },
 
+    /**
+     * Dessine la pomme sur le canvas.
+     * Utilise les coordonnées générées pour positionner la pomme.
+     * @function
+     */
     dessinePomme() {
         ctx.fillStyle = 'red';
         ctx.strokeStyle = 'darkred';
@@ -147,63 +196,70 @@ const app = {
         ctx.fill();
         ctx.stroke();
     },
-    
 
+    /**
+     * Affiche la fenêtre pop-up "Game Over".
+     * Permet de rejouer ou de quitter le jeu.
+     * @param {number} score - Le score du joueur.
+     * @function
+     */
     afficherGameOver(score) {
-        // Création de la fenêtre pop-up
         const popup = document.createElement('div');
         popup.classList.add('popup');
 
-        // Contenu de la fenêtre pop-up
+        // Message "Game Over"
         const message = document.createElement('h2');
         message.textContent = 'Game Over !';
         popup.appendChild(message);
 
+        // Affiche le score final
         const scoreText = document.createElement('h3');
         scoreText.textContent = 'Votre Score : ' + score + ' ' + 'Bravo !';
         popup.appendChild(scoreText);
 
+        // Bouton pour rejouer
         const btnRestart = document.createElement('button');
         btnRestart.textContent = 'Rejouer';
         btnRestart.addEventListener('click', function () {
-            // Action à effectuer lors du clic sur le bouton "Rejouer"
-
-            location.reload();
+            location.reload();  // Recharge la page pour rejouer
             app.init();
-
         });
         popup.appendChild(btnRestart);
 
+        // Bouton pour quitter
         const btnQuit = document.createElement('button');
         btnQuit.textContent = 'Quitter';
         btnQuit.addEventListener('click', function () {
-            // Action à effectuer lors du clic sur le bouton "Quitter"
-
-            app.fermerPopup();
+            app.fermerPopup();  // Ferme la fenêtre pop-up
         });
         popup.appendChild(btnQuit);
 
         // Ajout de la fenêtre pop-up au document
         document.body.appendChild(popup);
-
-        
     },
 
-    // Fonction pour fermer la fenêtre pop-up
+    /**
+     * Ferme la fenêtre pop-up "Game Over".
+     * @function
+     */
     fermerPopup() {
         const popup = document.querySelector('.popup');
         document.body.removeChild(popup);
     },
 
+    /**
+     * Vérifie si le jeu est terminé.
+     * Retourne true si le serpent touche les murs ou se mord lui-même.
+     * @returns {boolean} - Retourne true si le jeu est terminé, sinon false.
+     */
     finDuJeu() {
-
         let snakeSansTete = snake.slice(1, -1);
         let mordu = false;
         snakeSansTete.forEach(morceau => {
             if (morceau.x === snake[0].x && morceau.y === snake[0].y) {
                 mordu = true;
             }
-        })
+        });
 
         const toucheMurGauche = snake[0].x < -1;
         const toucheMurDroite = snake[0].x > canvas.width - 10;
@@ -213,68 +269,77 @@ const app = {
         let gameOver = false;
 
         if (mordu || toucheMurGauche || toucheMurDroite || toucheMurTop || toucheMurBottom) {
-            console.log('5')
             gameOver = true;
         }
-
 
         return gameOver;
     },
 
+    /**
+     * Bascule entre pause et reprise du jeu.
+     * Annule l'animation si le jeu est en pause et la reprend sinon.
+     * @function
+     */
     togglePause() {
         app.paused = !app.paused;
         if (app.paused) {
-            cancelAnimationFrame(app.animationId);
+            cancelAnimationFrame(app.animationId);  // Annule l'animation si le jeu est en pause
         } else {
-            app.animationId = requestAnimationFrame(app.animation.bind(this));
+            app.animationId = requestAnimationFrame(app.animation.bind(this));  // Reprend l'animation
         }
     },
 
+    /**
+     * Affiche la fenêtre pop-up de démarrage.
+     * Permet de lancer le jeu.
+     * @function
+     */
     demarrageDuJeu() {
-        // Création de la fenêtre pop-up
         const beguinPopup = document.createElement('div');
         beguinPopup.classList.add('beguinPopup');
 
-        // Contenu de la fenêtre pop-up
+        // Message "Lancer le jeu"
         const messageDemarrage = document.createElement('h4');
         messageDemarrage.textContent = 'Lancer le jeu';
         beguinPopup.appendChild(messageDemarrage);
 
+        // Bouton pour commencer à jouer
         const btnStart = document.createElement('button');
         btnStart.textContent = 'Jouer';
         btnStart.addEventListener('click', function () {
-            // Action à effectuer lors du clic sur le bouton "Jouer"
-
-            app.init();
-            app.fermerBeguinPopup();
-
+            app.init();  // Initialise le jeu
+            app.fermerBeguinPopup();  // Ferme la fenêtre pop-up de démarrage
         });
         beguinPopup.appendChild(btnStart);
 
         // Ajout de la fenêtre pop-up au document
         document.body.appendChild(beguinPopup);
-
-        
     },
 
-    // Fonction pour fermer la fenêtre pop-up
+    /**
+     * Ferme la fenêtre pop-up de démarrage.
+     * @function
+     */
     fermerBeguinPopup() {
         const beguinPopup = document.querySelector('.beguinPopup');
         document.body.removeChild(beguinPopup);
     },
 
-
-
-
+    /**
+     * Initialise et démarre le jeu.
+     * Configure le jeu et démarre l'animation.
+     * @function
+     */
     init() {
-        app.initialiseJeu();
-        app.creerPomme();
-        app.animation();
-        app.dessineLeSerpent();
-        app.handleKeydown();
+        app.initialiseJeu();  // Initialise les variables du jeu
+        app.creerPomme();  // Crée la première pomme
+        app.animation();  // Démarre l'animation du jeu
+        app.dessineLeSerpent();  // Dessine le serpent initial
+        app.handleKeydown();  // Gère les entrées clavier pour la direction
         const pauseButton = document.getElementById('pause-button');
-        pauseButton.addEventListener('click', app.togglePause.bind(app));
+        pauseButton.addEventListener('click', app.togglePause.bind(app));  // Gère le bouton de pause
     },
 }
 
-app.demarrageDuJeu()
+// Démarre le jeu en affichant la fenêtre pop-up de démarrage
+app.demarrageDuJeu();
